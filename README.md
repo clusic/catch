1f174ad75806411850b6c619f08ea94bd4ffd959
evio <evio@vip.qq.com>
2018-11-07 12:52:23 +0800

    update

--- README.md
+++ README.md
@@ -30,3 +30,57 @@ function error(e) {
 - error {function} 错误处理代码
 
 > 如果没有定义error，则将`throw error`。callback函数自带一个回滚函数即`roll`。
+
+## In Clusic
+
+### onResponseError
+
+请求级别接管错误输出
+
+```javascript
+// app.bootstrap.js
+module.exports = app => {
+  app.use(async (ctx, next) => {
+    ctx.onResponseError(error => {
+      ctx.status = error.status || 500;
+      ctx.body = {
+        error: error.messasge
+      }
+    })
+  })
+}
+```
+
+### onErrorCatch
+
+绑定回滚物理事件
+
+```javascript
+fs.writeFileSync('/test.txt', 'hello world', 'utf8');
+ctx.onErrorCatch(() => fs.unlinkSync('/test.txt'));
+```
+
+### error
+
+生成一个带status的错误信息
+
+```javascript
+throw ctx.error('error', 503);
+// 相当于
+const err = new Error('error');
+err.status = 503;
+throw err;
+```
+
+### sendResult
+
+发送成功结果
+
+```javascript
+ctx.sendResult({
+  a: 1
+}, 126000);
+// 相当于
+ctx.body = { status: 126000, data: { a: 1 } };
+ctx.status = 200;
+```
--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const MakeCatch = require('./index');
 const MakeError = require('./error');
+const { EventEmitter } = require('events');
 module.exports = async app => {
   app.MakeCatch = MakeCatch;
   app.use(async (ctx, next) => {
+    const event = new EventEmitter();
+
     await MakeCatch(async roll => {
-      defineReactive(ctx, 'Rollback', roll);
+      defineReactive(ctx, 'onResponseError', callback => event.on('error catch', callback));
+      defineReactive(ctx, 'onErrorCatch', roll);
       defineReactive(ctx, 'error', MakeError);
-      defineReactive(ctx, 'sendResult', (data, status) => {
-        ctx.body = { status: status || 200, data };
-        ctx.status = 200;
-      });
+      defineReactive(ctx, 'sendResult', sendResult(ctx));
       await next();
-    }, error => {
-      ctx.body = {
-        status: error.status || 500,
-        message: error.message
-      };
-      ctx.status = 200;
+    }, onError);
+
+    function onError(error) {
+      if (event.listenerCount('error catch') > 0) {
+        event.emit('error catch', error);
+      } else {
+        ctx.status = 200;
+        ctx.body = {
+          status: error.status || 500,
+          message: error.message
+        };
+      }
       ctx.app.Logger.error(error);
-    })
+    }
   });
 };
 
@@ -28,4 +35,11 @@ function defineReactive(ctx, name, target) {
       return target;
     }
   })
+}
+
+function sendResult(ctx) {
+  return (data, status) => {
+    ctx.body = { status: status || 200, data };
+    ctx.status = 200;
+  }
 }
